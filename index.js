9970191b62e161ea901296e7e313fe2f6f7585c7
maryam0007 <maryammicroverse@gmail.com>
2023-05-22 22:45:30 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+/** @format */
+import { getFromLocalStorage, setLocalStorage } from './local_storage.js';
+import Todo from './operation.js';
+import './style.css';
+
+const btnAdd = document.querySelector('.btn-add');
+const tasksList = document.querySelector('.list');
+const btnClear = document.querySelector('.btn-clear');
+
+const todo = new Todo();
+
+document.addEventListener('DOMContentLoaded', () => {
+  todo.displayTasks();
+});
+
+btnAdd.addEventListener('click', todo.addTask);
+
+tasksList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetId = e.target.getAttribute('id');
+    todo.removeTask(targetId);
+  } else if (e.target.closest('.disc')) {
+    const disc = e.target.closest('.disc');
+    disc.addEventListener('keyup', () => {
+      const tasks = getFromLocalStorage();
+      const index = +disc.id;
+      const task = tasks.find((task) => task.index === index);
+      task.description = disc.value.trim();
+      setLocalStorage(tasks);
+    });
+  } else if (e.target.classList.contains('checkbox')) {
+    const checkbox = document.querySelectorAll('.checkbox');
+    checkbox.forEach((item) => {
+      item.addEventListener('change', (e) => {
+        const targetId = e.target.getAttribute('id');
+        if (e.target.checked) {
+          todo.complete(targetId, true);
+        } else {
+          todo.complete(targetId, false);
+        }
+      });
+    });
+  }
+});
+btnClear.addEventListener('click', () => {
+  todo.clearAll();
+});
